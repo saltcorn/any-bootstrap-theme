72fa756d5e07d5ebbcb9fe0ba6a8250149397e91
Tom Nielsen <tanielsen@gmail.com>
2022-04-11 17:30:46 +0200

    avoid double sidebar

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ const wrapIt = (config, bodyAttr, headers, title, body) => {
       : ""
   }>
     ${body}
-    <link rel="stylesheet" href="/plugins/public/any-bootstrap-theme/sidebar-1.css" />
+    <link rel="stylesheet" href="/plugins/public/any-bootstrap-theme/sidebar-2.css" />
     ${
       features && features.deep_public_plugin_serve
         ? '<link rel="stylesheet" href="/plugins/public/any-bootstrap-theme/fontawesome/fontawesome.min.css" />'
