e18272cca0f709c74b750c857778a53ee20cb8f3
Tom Nielsen <tanielsen@gmail.com>
2022-08-13 15:09:03 +0100

    rm popper as  is already in bs bundle

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ const wrapIt = (config, bodyAttr, headers, title, body) => {
     <script src="/static_assets/${db.connectObj.version_tag
     }/jquery-3.6.0.min.js"></script>
     ${features && features.bootstrap5
-      ? `<script src="/plugins/public/any-bootstrap-theme/bootstrap.bundle.min.js"></script><script src="/plugins/public/any-bootstrap-theme/popper.min.js"></script>`
+      ? `<script src="/plugins/public/any-bootstrap-theme/bootstrap.bundle.min.js"></script>`
       : `
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>`
