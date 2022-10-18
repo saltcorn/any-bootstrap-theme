10e88e8117f0efa493e57dcad4424c3168aa0453
christianhugoch <hugo.christian@web.de>
2022-10-18 17:15:31 +0200

    set <base href> on mobile (#6)

--- index.js
+++ index.js
@@ -135,6 +135,7 @@ const wrapIt = (config, bodyAttr, headers, title, body) => {
   return `<!doctype html>
 <html lang="en">
   <head>
+    ${!isNode ? `<base href="http://localhost">` : ""}
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
