5a6a87b0c4f06a0fe3fa66d1fbb4a81e273fab1a
Christian Hugo <hugo.christian@web.de>
2024-06-18 11:33:03 +0200

    no mobile base href

--- index.js
+++ index.js
@@ -170,7 +170,6 @@ const wrapIt = (config, bodyAttr, headers, title, body) => {
   return `<!doctype html>
 <html lang="en" data-bs-theme="${config.mode || "light"}">
   <head>
-    ${!isNode ? `<base href="http://localhost">` : ""}
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
